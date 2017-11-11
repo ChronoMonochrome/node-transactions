aa5000864c8e524eefe3b76f8889229d24b0443c
Shilin Victor <chrono.monochrome@gmail.com>
2017-11-11 15:53:41 +0300

    Update config files

---
+++ src/app/index.module.js
@@ -0,0 +1,21 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('ngmkdev', ['ngAnimate', 'ngCookies', 'ngMessages', 'restangular', 
+            'ui.router', 'ui.bootstrap', 'toastr'])
+      .config(function ($stateProvider, $urlRouterProvider, RestangularProvider) {
+        $stateProvider
+        .state('transactions', {
+          url: "/transactions",
+          templateUrl: "app/main/transactions.html"
+        })
+        .state('settings', {
+          url: "/settings",
+          templateUrl: "app/settings/settings.html"
+        });
+        RestangularProvider.setBaseUrl("http://localhost:4567");
+
+        $urlRouterProvider.otherwise('/transactions');
+      });
+})();
