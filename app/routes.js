76420973e4bf39911249f1236ce24ffb010e866e
Shilin Victor <chrono.monochrome@gmail.com>
2017-11-14 17:10:56 +0300

    Simplify route scripts inclusion

--- app/routes.js
+++ app/routes.js
@@ -1,23 +1,6 @@
-// Initialize ALL routes including subfolders
-var fs = require('fs');
 var path = require('path');
 
-function recursiveRoutes(app, folderName) {
-    console.log(folderName);
-    fs.readdirSync(folderName).forEach(function(file) {
-
-        var fullName = path.join(folderName, file);
-        var stat = fs.lstatSync(fullName);
-
-        if (stat.isDirectory()) {
-            recursiveRoutes(fullName);
-        } else if (file.toLowerCase().indexOf('.js')) {
-            require(fullName)(app);
-            console.log("require('" + fullName + "')");
-        }
-    });
-}
-
 module.exports = function(app) {
-    recursiveRoutes(app, path.join(__dirname, 'routes')); // Initialize it
+    require(path.join(__dirname, 'routes/basic.js'))(app);
+    require(path.join(__dirname, 'routes/api.js'))(app);
 }
